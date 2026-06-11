e1ab9344c0cac9d5eabcb0d0ef1a46710141ac2c
iryna <irapart1607@gmail.com>
2026-06-11 18:00:38 +0400

    initiated app and cafe info components

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import css from './App.module.css';
+import CafeInfo from '../CafeInfo/CafeInfo';
+
+export default function App(){
+    return (
+        <div className={css.app}>
+            <CafeInfo />
+        </div>
+    )
+}
