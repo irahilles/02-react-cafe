8b2e17d1db43211aa8001dd924f16ee8a611b6d1
iryna <irapart1607@gmail.com>
2026-06-18 11:57:23 +0400

    rename

--- src/components/Folder-App/App.tsx
+++ src/components/Folder-App/App.tsx
@@ -1,10 +1,10 @@
 import css from './App.module.css';
-import CafeInfo from '../cafeInfo/CafeInfo';
+import CafeInfo from '../FolderCafeInfo/CafeInfo';
 import { useState } from 'react';
 import type { Votes, VoteType} from '../../types/votes';
-import VoteOptions from '../voteOptions/VoteOptions';
-import VoteStats from '../voteStats/VoteStats';
-import Notification from '../notification/Notification';
+import VoteOptions from '../FolderVoteOptions/VoteOptions';
+import VoteStats from '../VoteStatsF/VoteStats';
+import Notification from '../Not-folder/Notification';
 
 export default function App(){
     const [votes, setVotes] = useState<Votes>({
